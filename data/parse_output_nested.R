2c24b69c8a63ccd664d5c16299c8ae15ad402dda
Sean Devine <seandamiandevine@gmail.com>
2023-06-23 13:05:47 -0400

    fix error in number of parameters for model comparison with ic uncertainty

--- data/parse_output_nested.R
+++ data/parse_output_nested.R
@@ -1,14 +1,59 @@
 
 rm(list=ls())
 
+select_model = function(vals, ses=NULL, u=0, ic='DIC', 
+                        n_pars= c(6, 5, 10, 6, 8) ) {
+  #' Select a model based on fit index and uncertainty
+  
+  if(u==0) {
+    return(as.numeric(which.min(vals)))
+  }
+  
+  # pick best two
+  m1 = which.min(vals)
+  m2 = which.min(vals[-m1])
+  m2 = which(names(vals)==names(m2)) # fix index
+  
+  # check if these differ based on uncertainty
+  # (d==TRUE, if difference is robust)
+  if(ic=='DIC') {
+    d = (vals[m2] - vals[m1]) >= u
+  } else {
+    ci_1 = c(vals[m1]-u*ses[m1], vals[m1]+u*ses[m1])
+    ci_2 = c(vals[m2]-u*ses[m2], vals[m2]+u*ses[m2])
+    d = !(ci_1[1] <= ci_2[2]) && (ci_1[2] >= ci_2[1])
+  }
+  
+  # pick best and/or most concise model
+  if(d) {
+    win = m1
+  } else {
+    # choose simplest model if CIs overlap
+    # if both models are equally simple, 
+      # choose model with best fitting point estimate
+    win = ifelse(n_pars[m2] < n_pars[m1], m2, m1)
+  }
+  win
+}
+
+# Load files and prepare df -----------------------------------------------
+
 rds_files = list.files('raw_data/',pattern='.rds',full.names = T)
 n_samples = 100
-
 conds = read.csv('conds_with_var.csv')[,c('N','J','TAU0','G10',
                                           'TAU1','G20','SIG2')]
-
 colnames = c('cond','iter',
              names(conds), 
+             'g0s',
+             'g1s',
+             'g2s',
+             't0s',
+             't1s',
+             'd_g0s',
+             'd_g1s',
+             'd_g2s',
+             'd_t0s',
+             'd_t1s',
              'max_rhats',
              'min_ess',
              'DIC',
@@ -19,14 +64,16 @@ colnames = c('cond','iter',
              'DIC_elpd',
              'WAIC_elpd',
              'LOO_elpd',
-             paste0('win_',c('DIC','WAIC','LOO'))
+             paste0('win_',c('DIC','WAIC','LOO')),
+             paste0('win_1se_',c('DIC','WAIC','LOO'))
              )
-             
 
 # preallocate
 df  = matrix(NA, length(rds_files)*n_samples, length(colnames), 
              dimnames = list(NULL, colnames))
 df = as.data.frame(df)
+
+# iterate -----------------------------------------------------------------
 
 row = 1
 pb = txtProgressBar(1,nrow(df))
@@ -42,7 +89,31 @@ for(f in rds_files) {
   
   for(i in 1:length(x)) {
     
-    # point estimates
+    # mean gammas per model
+    g0s = sapply(x[[i]]$fixef, function(j) j['Intercept','Estimate'])
+    g1s = sapply(x[[i]]$fixef, function(j) j['X1','Estimate'])
+    g2s = sapply(x[[i]]$fixef, function(j) {
+      if('X2' %in% rownames(j)) return(j['X2','Estimate'])
+      return(NA)
+    })
+    
+    # difference from true value
+    d_g0s = 1-g0s
+    d_g1s = conds_x[['G10']] - g1s
+    d_g2s = conds_x[['G20']] - g2s
+    
+    # mean taus per model
+    t0s = sapply(x[[i]]$ranef, function(j) j$g$sd['Intercept','Estimate'])
+    t1s = sapply(x[[i]]$ranef, function(j) {
+      if('X1' %in% rownames(j$g$sd)) return(j$g$sd['X1','Estimate'])
+      return(NA)
+    })
+    
+    # difference from true value
+    d_t0s = conds_x[['TAU0']] - t0s^2
+    d_t1s = conds_x[['TAU1']] - t1s^2
+    
+    # point estimates of ic
     dics  = sapply(x[[i]]$dic,function(j) j[['DIC']])
     waics = sapply(x[[i]]$waic,function(j) j['waic','Estimate'])
     loos  = sapply(x[[i]]$loo,function(j) j['looic','Estimate'])
@@ -57,9 +128,14 @@ for(f in rds_files) {
     loos_elpd  = sapply(x[[i]]$loo,function(j) j['elpd_loo','Estimate'])
     
     # lowest estimate
-    winning_dic  = as.numeric(which.min(dics))
-    winning_waic = as.numeric(which.min(waics))
-    winning_loos = as.numeric(which.min(loos))
+    winning_dic  = select_model(dics)
+    winning_waic = select_model(waics)
+    winning_loos = select_model(loos)
+    
+    # lowest estimate with uncertainty incorporated
+    winning_1se_dic  = select_model(dics, u = 4, ic = 'DIC')
+    winning_1se_waic = select_model(waics, ses = waics_e, u = 1, ic = 'WAIC')
+    winning_1se_loos = select_model(loos,  ses = loos_e,  u = 1, ic = 'LOO')
     
     # summary stats on diagnostics
     max_rhat = as.vector(sapply(x[[i]]$diag, function(j) max(j$Rhat)))
@@ -69,6 +145,16 @@ for(f in rds_files) {
     df[row,] = c(cond_num, 
                  i,
                  unlist(conds_x), 
+                 list(list(g0s)),
+                 list(list(g1s)),
+                 list(list(g2s)),
+                 list(list(t0s)),
+                 list(list(t1s)),
+                 list(list(d_g0s)),
+                 list(list(d_g1s)),
+                 list(list(d_g2s)),
+                 list(list(d_t0s)),
+                 list(list(d_t1s)),
                  list(list(max_rhat)),
                  list(list(min_ess)),
                  list(list(dics)),
@@ -81,14 +167,22 @@ for(f in rds_files) {
                  list(list(loos_elpd)),
                  winning_dic, 
                  winning_waic, 
-                 winning_loos
-    )
+                 winning_loos,
+                 winning_1se_dic, 
+                 winning_1se_waic, 
+                 winning_1se_loos
+                 )
     
     row = row+1
     
   } # end of iter loop
 } # end of file loop
 
-# save output
+
+
+# save --------------------------------------------------------------------
+
+
 saveRDS(df, 'fits_summary.rds')
+
 
